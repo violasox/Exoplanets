273f140840d63fdc29970606c7b90b5612adbf8c
Kaylene <viola.sox@gmail.com>
2019-04-22 12:06:40 -0400

    Better organization, UV flux distribution

--- find_overlap.py
+++ find_overlap.py
@@ -3,24 +3,51 @@ from astropy.io import fits
 from astropy.table import Table
 import string
 import numpy as np
+import pickle
+
+class Exoplanet():
+    def __init__(self):
+        self.uv = None
+        self.dist = None
+        self.mass = None
+        self.radius = None
 
 exoplanets = ascii.read('exoplanets.csv')
 stars = fits.open('TD1.fits')
 stars = Table(stars[1].data)
 
-print(exoplanets.info())
-print(stars.info())
+# print(exoplanets.info())
+# print(stars.info())
 starDict = {}
 for i, starNum in enumerate(stars['HD_NUMBER']):
     starDict[str(starNum)] = i
 
 overlapCount = 0
-indices = ~np.ma.getmask(exoplanets['star_name'])
-for hostStarName in exoplanets['star_name'][indices]:
-    print(hostStarName)
-    if hostStarName.startswith('HD'):
-        starNum = str(hostStarName.replace('HD ', ''))
-        if starNum in starDict:
-            overlapCount += 1
+indices = np.where(~np.ma.getmask(exoplanets['star_name']))[0]
+altIndices = np.where(~np.ma.getmask(exoplanets['star_alternate_names']))[0]
+foundExoplanets = []
+for index in indices:
+    starNames = []
+    starNames.append(str(exoplanets[index]['star_name']))
+    if index in altIndices:
+        newNames = exoplanets['star_alternate_names'][index].split(', ')
+        starNames += newNames
+    for name in starNames:
+        if name.startswith('HD'):
+            starNum = str(name.replace('HD ', ''))
+            if starNum in starDict:
+                starIndex = starDict[starNum]
+                overlapCount += 1
+                e = Exoplanet()
+                star = stars[starIndex]
+                e.uv = [star['FLUX_1565_A'], star['FLUX_1965_A'], star['FLUX_2365_A'], 
+                        star['FLUX_2740_A']]
+                e.dist = exoplanets[index]['star_distance']
+                e.mass = exoplanets[index]['mass']
+                e.radius = exoplanets[index]['radius']
+                foundExoplanets.append(e)
 
-print(overlapCount)
+if __name__ == '__main__':
+    with open ('project.dat', 'wb') as f:
+        pickle.dump(foundExoplanets, f, pickle.HIGHEST_PROTOCOL)
+    print(overlapCount)
---
+++ uv_distribution.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib
+from matplotlib import pyplot
+import pickle
+from find_overlap import Exoplanet
+
+matplotlib.rcParams.update({'font.size': 18})
+
+with open('project.dat', 'rb') as f:
+    foundExoplanets = pickle.load(f)
+
+uvBand = []
+uvMag = []
+for exoplanet in foundExoplanets:
+    uvBand += [0, 1, 2, 3]
+    uvMag += exoplanet.uv
+
+pyplot.semilogy(uvBand, uvMag, 'bo', alpha=0.5)
+pyplot.ylabel('UV Flux [$erg cm^{-2} s^{-1} A^{-1}$]')
+pyplot.xticks([0,1,2,3], [1565,1965,2365,2740])
+pyplot.xlabel('UV band [A]')
+pyplot.title('UV flux of stars with known exoplanets')
+pyplot.tight_layout()
+pyplot.savefig('uv_distribution.pdf')
+pyplot.show()
